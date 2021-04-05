22158c1dcef8d202a5dd0aac2b63583e18c40cd7
Vivek <vivek.159357@gmail.com>
2021-04-05 21:40:22 +0530

    Added user profile feed API

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.models import BaseUserManager
+from django.conf import settings
 
 
 class UserProfileManager(BaseUserManager):
@@ -11,22 +12,22 @@ class UserProfileManager(BaseUserManager):
         """Create a new user profile"""
         if not email:
             raise ValueError('User must have an email address')
-        
+
         email = self.normalize_email(email)
-        user = self.model(email = email, name = name)
+        user = self.model(email=email, name=name)
 
         user.set_password(password)
-        user.save(using = self._db)
+        user.save(using=self._db)
 
         return user
 
     def create_superuser(self, email, name, password):
         """Create and save a new superuser with given details"""
         user = self.create_user(email, name, password)
-        
+
         user.is_superuser = True
         user.is_staff = True
-        user.save(using = self._db)
+        user.save(using=self._db)
 
         return user
 
@@ -35,7 +36,7 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
     """Database model for users in the system"""
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
-    is_activate = models.BooleanField(default = True)
+    is_activate = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
     objects = UserProfileManager()
@@ -55,7 +56,19 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
         """Return string representation of our user"""
         return self.email
 
-        
 
+class ProfileFeedItem(models.Model):
+    """Profile status update"""
+    user_profile = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
 
+    status_text = models.CharField(max_length=255)
+    created_on = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        """Return the model as a string"""
+        return self.status_text
+    
+    
 
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -6,6 +6,7 @@ from rest_framework.authentication import TokenAuthentication
 from rest_framework import filters
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
+from rest_framework.permissions import IsAuthenticated
 
 
 from profiles_api import serializers
@@ -47,7 +48,7 @@ class HelloApiView(APIView):
         return Response({'method': 'PUT'})
 
     def patch(self, request, pk=None):
-        """Handle a partila update of an object"""
+        """Handle a partial update of an object"""
         return Response({'method': "PATCH"})
 
     def delete(self, request, pk=None):
@@ -110,7 +111,19 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name', 'email', )
 
+
 class UserLoginApiView(ObtainAuthToken):
     """Handle creating user authentication tokens"""
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
-    
+
+
+class UserProfileFeedViewSet(viewsets.ModelViewSet):
+    """Handles creating, reading and updating profile feed items"""
+    authentication_classes = (TokenAuthentication,)
+    serializer_class = serializers.ProfileFeedItemSerializer
+    queryset = models.ProfileFeedItem.objects.all()
+    permission_classes = (permissions.UpdateOwnStatus, IsAuthenticated)
+
+    def perform_create(self, serializer):
+        """Sets the user profile to the logged in user"""
+        serializer.save(user_profile=self.request.user)
